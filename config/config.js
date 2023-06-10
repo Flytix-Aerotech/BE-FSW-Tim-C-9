9d7b9ab3660a1076485047279be249bdd40f5f14
Imam Taufiq Hermawan <imamtaufiq133@gmail.com>
2023-06-10 19:35:46 +0700

    setup port config

--- config/config.js
+++ config/config.js
@@ -8,8 +8,8 @@ module.exports = {
     password: DB_PASSWORD,
     database: `${DB_NAME}_development`,
     host: DB_HOST,
-    dialect: DB_DIALECT,
     port: DB_PORT,
+    dialect: DB_DIALECT,
   },
   test: {
     username: DB_USERNAME,
