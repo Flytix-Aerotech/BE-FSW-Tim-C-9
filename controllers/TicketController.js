a8fc1b56b25535fb77193f1f9408e4b6fb5b3ea0
theresianwg <theresianawangsih@gmail.com>
2023-06-11 23:51:26 +0700

    search ticket

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -47,79 +47,93 @@ const getTicketById = async (req, res) => {
     }
 };
 
-const addTicket = async (req, res) => {
-    try {
-        const newTicket = await ticket.create(req.body);
-        res.status(200).json({
-            message: 'tiket berhasil ditambahkan',
-            newTicket,
-        });
-    } catch (error) {
-        res.status(error.statusCode || 500).json({
-            message: error.message,
-        });
-    }
-};
+// const addTicket = async (req, res) => {
+//     try {
+//         const newTicket = await ticket.create(req.body);
+//         res.status(200).json({
+//             message: 'tiket berhasil ditambahkan',
+//             newTicket,
+//         });
+//     } catch (error) {
+//         res.status(error.statusCode || 500).json({
+//             message: error.message,
+//         });
+//     }
+// };
 
-const updateTicket = async (req, res) => {
-    try {
-        const { id, } = req.params;
-        await ticket.update(req.body, { where: { id, }, });
-        res.status(200).json({
-            message: 'tiket berhasil diubah',
-        });
-    } catch (error) {
-        res.status(error.statusCode || 500).json({
-            message: error.message,
-        });
-    }
-};
+// const updateTicket = async (req, res) => {
+//     try {
+//         const { id, } = req.params;
+//         await ticket.update(req.body, { where: { id, }, });
+//         res.status(200).json({
+//             message: 'tiket berhasil diubah',
+//         });
+//     } catch (error) {
+//         res.status(error.statusCode || 500).json({
+//             message: error.message,
+//         });
+//     }
+// };
 
-const deleteTicket = async (req, res) => {
-    try {
-        const { id, } = req.params;
-        await ticket.destroy({ where: { id, }, });
-        res.status(200).json({
-            message: 'tiket berhasil dihapus',
-        });
-    } catch (error) {
-        res.status(error.statusCode || 500).json({
-            message: error.message,
-        });
-    }
-};
+// const deleteTicket = async (req, res) => {
+//     try {
+//         const { id, } = req.params;
+//         await ticket.destroy({ where: { id, }, });
+//         res.status(200).json({
+//             message: 'tiket berhasil dihapus',
+//         });
+//     } catch (error) {
+//         res.status(error.statusCode || 500).json({
+//             message: error.message,
+//         });
+//     }
+// };
 
+// pencarian + hasil tiket
 const searchTicket = async (req, res) => {
     try {
-        const { departure_date, arrival_date, class_type, price, airport_name, airport_location } = req.query;
+        const {departure_date, arrival_date, departure_location,  arrival_location,  airport_name, airport_location, type_of_class, price} = req.query;
         const tickets = await ticket.findAll({
             where: {
                 [Op.or]: [
                     {
-                        '$airport.airport_location$': { [Op.iLike]: `%${airport_location}%` },
+                        '$flight.departure_date$': { [Op.iLike]: `%${departure_date}%` },
                     },
                     {
-                        '$airport.airport_name$': { [Op.iLike]: `%${airport_name}%` },
+                        '$flight.arrival_date$': { [Op.iLike]: `%${arrival_date}%` },
                     },
                     {
-                        departure_date,
+                        '$flight.departure_location$': { [Op.iLike]: `%${departure_location}%` },
                     },
                     {
-                        arrival_date,
+                        '$flight.arrival_location$': { [Op.iLike]: `%${arrival_location}%` },
                     },
                     {
-                        class: { [Op.iLike]: `%${class_type}%` },
+                        '$airport.name$': { [Op.iLike]: `%${airport_name}%` },
+                    },
+                    {
+                        '$airport.location$': { [Op.iLike]: `%${airport_location}%` },
+                    },
+                    {
+                        type_of_class : { [Op.iLike]: type_of_class },
                     },
                     {
                         price: { [Op.gt]: price},
-                    },
+                    }, 
                 ]
             },
-            include: [{
-                model: airport,
-                as: 'airport',
-                required: true
-            }]
+            include: [
+                {
+                    model: airport,
+                    as: 'airport',
+                    required: true
+                },
+                {
+                    model: flight,
+                    as: 'flight',
+                    required: true
+                }
+            ]
         });
         res.status(200).json({
             tickets
@@ -135,8 +149,8 @@ const searchTicket = async (req, res) => {
 module.exports = {
     getTicket,
     getTicketById,
-    addTicket,
-    updateTicket,
-    deleteTicket,
+    // addTicket,
+    // updateTicket,
+    // deleteTicket,
     searchTicket,
 };
