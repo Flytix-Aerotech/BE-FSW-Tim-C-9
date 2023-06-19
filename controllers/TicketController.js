e593634fba9045a554f4235f43a87f741e6a4930
theresianwg <theresianawangsih@gmail.com>
2023-06-20 01:28:25 +0700

    done all ticket (search without dd & ad)

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -51,6 +51,33 @@ const getTicketById = async (req, res) => {
   }
 };
 
+const addTicket = async (req, res) => {
+  try {
+      const newTicket = await ticket.create(req.body);
+      res.status(200).json({
+          message: 'tiket berhasil ditambahkan',
+          newTicket,
+      });
+  } catch (error) {
+      res.status(error.statusCode || 500).json({
+          message: error.message,
+      });
+  }
+};
+const updateTicket = async (req, res) => {
+  try {
+      const { id, } = req.params;
+      await ticket.update(req.body, { where: { id, }, });
+      res.status(200).json({
+          message: 'tiket berhasil diubah',
+      });
+  } catch (error) {
+      res.status(error.statusCode || 500).json({
+          message: error.message,
+      });
+  }
+};
+
 const filterTicket = async (req, res) => {
   const { departure_date, arrival_date, departure_location, arrival_location, type_of_class } = req.body;
   try {
@@ -118,12 +145,26 @@ const searchTicket = async (req, res) => {
   }
 };
 
+const deleteTicket = async (req, res) => {
+  try {
+      const { id, } = req.params;
+      await ticket.destroy({ where: { id, }, });
+      res.status(200).json({
+          message: 'tiket berhasil dihapus',
+      });
+  } catch (error) {
+      res.status(error.statusCode || 500).json({
+          message: error.message,
+      });
+  }
+};
+
 module.exports = {
   getTicket,
   getTicketById,
-  //   addTicket,
-  //   updateTicket,
-  //   deleteTicket,
+  addTicket,
+  updateTicket,
+  deleteTicket,
   filterTicket,
   searchTicket,
 };
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
 const Auth = require("../middleware/Auth");
-const { getTicket, getTicketById, filterTicket, searchTicket } = require("../controllers/TicketController");
+const { getTicket, getTicketById, filterTicket, searchTicket, addTicket, updateTicket,deleteTicket } = require("../controllers/TicketController");
 
 router.get("", getTicket);
 router.get("/search", searchTicket);
 router.get("/:id", getTicketById);
-
-// router.post("", TicketController.addTicket);
-// router.put("/:id", TicketController.updateTicket);
-// router.delete("/:id", TicketController.deleteTicket);
+router.put("/:id", updateTicket);
 router.post("/filter", filterTicket);
+router.post("", addTicket);
+
+router.delete("/:id", deleteTicket);
+
 
 
 module.exports = router;
